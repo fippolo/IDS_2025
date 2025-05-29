fb76e7b7d71513fa2e3df62e737dfa271fca944f
fippolo <filippo.fanesi@gmail.com>
2025-05-29 19:10:41 +0200

    Creazione progetto IDEA con springboot

---
+++ FilieraFanesiCardinali/src/main/java/unicam/filierafanesicardinali/FilieraFanesiCardinaliApplication.java
@@ -0,0 +1,13 @@
+package unicam.filierafanesicardinali;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilieraFanesiCardinaliApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FilieraFanesiCardinaliApplication.class, args);
+    }
+
+}
